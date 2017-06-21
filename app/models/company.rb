493fc7b6a02c881ece65a94356de1c6ff68a6ced
Alex Benoit <alexander.benoit@outlook.com>
2017-06-21 17:26:52 +0100

    removed uniqueneess on url

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,7 +2,8 @@ class Company < ApplicationRecord
   has_many :locations
   belongs_to :category
 
-  validates :name, :url, presence: true, uniqueness: { case_sensitive: false }
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
+  validates :url, presence: true
 
   before_validation :strip_company_name
 
