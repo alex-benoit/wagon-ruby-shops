511f53a52edf8e77f60af0141a6d9f2197b381c3
Arthur Littmann <arthur.littmann@gmail.com>
2017-06-21 17:17:13 +0100

    views with bugs

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,2 +1,41 @@
 class CompaniesController < ApplicationController
+  before_action :set_company, only: [:edit, :update]
+  def index
+    @companies = Company.all
+
+  end
+
+  def new
+    @company = Company.new
+  end
+
+  def edit
+  end
+
+  def create
+    @company = Company.new(product_params)
+    if @company.save
+      redirect_to companies_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @company.update(product_params)
+      redirect_to companies_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def set_company
+    @company = Company.find(params[:id])
+  end
+
+  def product_params
+    params.require(:company).permit(:name, :employees, :url)
+  end
 end
