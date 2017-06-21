2a35c240bdcf6273ff4259529867294c24a848a5
Alex Benoit <alexander.benoit@outlook.com>
2017-06-21 16:13:52 +0100

    added model validations

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,5 @@
 class Company < ApplicationRecord
   has_many :locations
+
+  validates :name, :url, presence: true
 end
--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,9 @@
 class Location < ApplicationRecord
+  geocoded_by :address
+
   belongs_to :company
+
+  validates :address, presence: true
+
+  after_validation :geocode, if: :address_changed?
 end
